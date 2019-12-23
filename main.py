dcd1d06ccd33f392b95f4d7420268714f79e9719
me@tyran.ru <Defender2018for>
2019-12-23 23:25:41 +0300

    v.4

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-# Мы устроились на новую работу. Бывший сотрудник начал разрабатывать модуль для работы с почтой,
-# но не успел доделать его. Код рабочий. Нужно только провести рефакторинг кода.
+# Мы устроились на новую работу.
+# Бывший сотрудник начал разрабатывать модуль для работы с почтой,
+# но не успел доделать его. Код рабочий.
+# Нужно только провести рефакторинг кода.
 # Создать класс для работы с почтой;
 # Создать методы для отправки и получения писем;
-# Убрать "захардкоженный" код. Все значения должны определяться как аттрибуты класса, либо аргументы методов;
+# Убрать "захардкоженный" код. Все значения должны определяться как
+# аттрибуты класса, либо аргументы методов;
 # Переменные должны быть названы по стандарту PEP8;
 # Весь остальной код должен соответствовать стандарту PEP8;
 
@@ -29,6 +32,7 @@ class MyEmail:
         self.recipients = recipients
         self.message = message
         self.header = header
+        self.smtp_port = 587
 
         msg = MIMEMultipart()
         msg['From'] = self.login
@@ -36,7 +40,7 @@ class MyEmail:
         msg['Subject'] = self.subject
         msg.attach(MIMEText(self.message))
 
-        ms = smtplib.SMTP(self.smtp_server, 587)
+        ms = smtplib.SMTP(self.smtp_server, self.smtp_port)
         # identify ourselves to smtp gmail client
         ms.ehlo()
         # secure our email with tls encryption
@@ -58,20 +62,24 @@ class MyEmail:
         mail.login(self.login, self.password)
         mail.list()
         mail.select(self.folder)
-        self.criterion = '(HEADER Subject "%s")' % self.header if self.header else 'ALL'
+        self.criterion = '(HEADER Subject "%s")' % self.header \
+            if self.header else 'ALL'
         result, data = mail.uid('search', None, self.criterion)
         assert data[0], 'There are no letters with current header'
         latest_email_uid = data[0].split()[-1]
         result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
         raw_email = data[0][1]
-        email_message = email.message_from_string(raw_email)
+        email_message = mail.message_from_string(raw_email)
         mail.logout()
 
 
 def main():
-    example_message = MyEmail('smtp.gmail.com', 'imap.gmail.com', 'login@gmail.com', 'qwerty')
-    # example_message.send_message('Subject', ['vasya@email.com', 'petya@email.com'], 'Message', None)
+    example_message = MyEmail('smtp.gmail.com', 'imap.gmail.com', \
+                              'login@gmail.com', 'qwerty')
+    # example_message.send_message('Subject', ['vasya@email.com', \
+    # 'petya@email.com'], 'Message', None)
     # example_message.receive_message("inbox", None)
+
 
 if __name__ == '__main__':
     main()
